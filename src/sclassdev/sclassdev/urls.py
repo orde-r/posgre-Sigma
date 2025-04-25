720b6d856677e9c552bc30b55975036f385dcfbd
danielson <danielsonhong@gmail.com>
2025-04-25 12:59:47 +0700

    done create

--- src/sclassdev/sclassdev/urls.py
+++ src/sclassdev/sclassdev/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_landing.urls')),
-    path('blog', include('app_blog.urls')),
+    path('blog/', include('app_blog.urls')),
 ]
