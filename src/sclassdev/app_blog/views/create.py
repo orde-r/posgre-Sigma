7224d028847fa01364ccd4bd2f23808023b250bb
danielson <danielsonhong@gmail.com>
2025-06-17 15:13:42 +0700

    syntax fixed

--- src/sclassdev/app_blog/views/create.py
+++ src/sclassdev/app_blog/views/create.py
@@ -6,7 +6,7 @@ def view(request):
         title = request.POST.get('title')
         content = request.POST.get('content')
 
-        result = query("INSERT INTO blog_post (title,content) VALUES (%s, %s)", (title,content))
+        result = query("INSERT INTO blog_post (title,content) VALUES (%s, %s)", [title,content])
         print(result)
 
     return render(request, 'app_blog/create.html')
